0cd258c162edb8b11f3165bc545a09c2adc42cf5
PayamEmami <payam.emami@scilifelab.se>
2022-11-13 18:04:54 +0100

    Update __main__.py

--- style_transfer/__main__.py
+++ style_transfer/__main__.py
@@ -107,7 +107,8 @@ def main():
     with Image.open(args.content) as content, Image.open(args.style) as style:
         if args.pyramid:
            shapes = []
-           cur_shape = compute_shape(content.shape, args.max_side)
+           image_shape=(content.size[0], content.size[1], 3)
+           cur_shape = compute_shape(image_shape, args.max_side)
            while max(cur_shape[0], cur_shape[1]) > 224:
             shapes = [cur_shape] + shapes
             cur_shape = (cur_shape[0] // 2, cur_shape[1] // 2, cur_shape[2])
